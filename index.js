f4290990b76f0fb25ae6facca37fe847b601069a
saileshgg <saileshgurung36@gmail.com>
2022-01-14 13:03:07 +0530

    css

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check-number");
+const output = document.querySelector("#output");
+
+
+function calculateSum(num) {
+    var sum = 0;
+
+    for(var i=0;i<num.length;i++) {
+        var ch = parseInt(num.charAt(i));
+
+        sum += ch;
+
+    }
+
+    return sum;
+}
+
+function isBirthdatLucky(sum) {
+    if(sum % luckyNumber.value === 0) {
+        output.innerText = "Your birthday is lucky";
+    } else {
+        output.innerText = "Not lucky";
+    }
+}
 
 checkNumber.addEventListener("click", function() {
-    console.log(dateOfBirth);
-    console.log(luckyNumber.value);
+    const modifiedDate = dateOfBirth.value.replaceAll("-","");
+    console.log(modifiedDate);
+    
+    let sum = calculateSum(modifiedDate);
+    console.log(sum);
+
+
+    isBirthdatLucky(sum);
+    
+
     console.log("Button clicked");
 })
+
